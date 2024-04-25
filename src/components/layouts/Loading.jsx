d866899b9133dc4734b80ce9dc8ea07fa1650f9c
saikat <tawhidulislam3482@gmail.com>
2024-04-25 19:18:56 +0600

    last commit

--- src/components/layouts/Loading.jsx
+++ src/components/layouts/Loading.jsx
@@ -1,12 +1,12 @@
 import loading from '../../assets/image/loading.gif';
 const Loading = () => {
-  const style = {
+  const styles = {
     background: `radial-gradient(circle, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 30%)`,
   };
 
   return (
     <div className="relative flex justify-center items-center h-screen w-full">
-      <div style={style} className=" h-full w-full absolute top-0"></div>
+      <div style={styles} className=" h-full w-full absolute top-0"></div>
       <img src={loading} alt="" />
     </div>
   );
