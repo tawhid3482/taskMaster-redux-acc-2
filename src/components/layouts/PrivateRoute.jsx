5f8e503d31a40c8a3d34e01dc63201e735c59021
saikat <tawhidulislam3482@gmail.com>
2024-04-26 10:31:33 +0600

    private route setup

--- src/components/layouts/PrivateRoute.jsx
+++ src/components/layouts/PrivateRoute.jsx
@@ -1,18 +1,28 @@
 import { Navigate, useLocation } from "react-router-dom";
 import Loading from "./Loading";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import auth from "../../utils/firebase.config";
+import { setUser, toggleLoading } from "../../redux/features/user/userSlice";
 
 const PrivateRoute = ({ children }) => {
   const { pathname } = useLocation();
-
+  const dispatch = useDispatch();
   const { email, isLoading } = useSelector((state) => state.userSlice);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      console.log(user);
+        if(user){
+          dispatch(setUser({
+            name: user.displayName,
+            email:user.email
+          }))
+          dispatch(toggleLoading(false))
+        }
+        else{
+          dispatch(toggleLoading(false))
+        }
     });
   }, []);
 
--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -27,7 +27,15 @@ export const createUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "userSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    setUser : (state,{payload})=>{
+      state.name = payload.name,
+      state.email = payload.email
+    },
+    toggleLoading : (state,{payload})=>{
+      state.isLoading = payload
+    }
+  },
   extraReducers: (builder) => {
     builder
       .addCase(createUser.pending, (state) => {
@@ -53,5 +61,5 @@ const userSlice = createSlice({
       });
   },
 });
-
+export const {setUser,toggleLoading}= userSlice.actions
 export default userSlice.reducer;
